3fc5431932ac04b142ab057891feb36207c5d907
Dmitry <3ddesign@bk.ru>
2022-05-18 08:49:34 +0200

    add convertion tests

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -14,3 +14,22 @@ it('should summiraze all values', () => {
     // Assert
     expect(result).toBe(expectedResult);
 });
+
+it('should yield if at least one invalid number is provided ', () => {
+    const inputs = ['invalid', 1];
+
+    const result = add(inputs);
+
+    expect(result).toBeNaN();
+});
+
+it('should yield a correct sum if an array of numeric is provided', () => {
+    const numbers = ['1', '2'];
+
+    const result = add(numbers);
+
+    const expectedResult = numbers.reduce(
+        (previous, current) => +previous + +current, 0);
+
+    expect(result).toBe(expectedResult);
+});
