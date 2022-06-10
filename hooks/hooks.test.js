5122ed345c09034bf2a108615dd9286c715f4f17
Dmitry <3ddesign@bk.ru>
2022-06-10 09:08:28 +0200

    add example async running with concurrent

--- hooks/hooks.test.js
+++ hooks/hooks.test.js
@@ -3,43 +3,43 @@ import { it, expect, beforeAll, beforeEach, afterEach, afterAll } from 'vitest';
 import { User } from './hooks';
 
 const testEmail = 'test@test.com';
-const user = new User(testEmail);
+let user;
 
 beforeAll(() => {
 
 });
 
 beforeEach(() => {
-  
+  user = new User(testEmail);
 });
 
 afterEach(() => {
-  
 });
 
 afterAll(() => {
   
 });
 
-it('should update the email', () => {
+it.concurrent('should update the email', () => {
   user.updateEmail(newTestEmail);
   expect(user.email).toBe(newTestEmail);
 });
 
-it('should have an email property', () => {
+it.concurrent('should have an email property', () => {
   expect(user).toHaveProperty('email');
 });
 
-it('should store the provided email value', () => {
+it.concurrent('should store the provided email value', () => {
   expect(user.email).toBe(testEmail);
 });
 
-it('should clear the email', () => {
+
+it.concurrent('should clear the email', () => {
   user.clearEmail();
   expect(user.email).toBe('');
 });
 
-it('should still have an email property after clearing the email', () => {
+it.concurrent('should still have an email property after clearing the email', () => {
   user.clearEmail();
   expect(user).toHaveProperty('email');
 });
