b7db21a65a26c8c328ed73a209aa49fd77666cdc
Dmitry <3ddesign@bk.ru>
2022-06-17 09:43:25 +0200

    testing with spy

---
+++ moking&spies/src/datat.test.js
@@ -0,0 +1,12 @@
+import { describe, it, expect, vi, } from 'vitest';
+import { generateReportData } from './data';
+
+describe('generateReportData()', () => {
+    it('it should execute logFn is provided', () => {
+        const logger = vi.fn();
+
+        generateReportData(logger);
+
+        expect(logger).toBeCalled();
+    });
+});
