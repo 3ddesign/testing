47fc66022b5fd2094f0c24c188db29862bfe477d
Dmitry <3ddesign@bk.ru>
2022-06-28 09:41:53 +0200

    test with mocking

--- moking&spies/src/util/io.test.js
+++ moking&spies/src/util/io.test.js
@@ -4,6 +4,15 @@ import { promices as fs } from 'fs';
 import writeData from './io';
 
 vi.mock('fs');
+vi.mock('path', () => {
+    return {
+        default: {
+            join: (...args) => {
+                return args[args.length - 1]
+            }
+        }
+    };
+});
 
 it('shoud exeucte the writeFile method', () => {
     const testData = 'Test';
@@ -12,5 +21,6 @@ it('shoud exeucte the writeFile method', () => {
     writeData(testData, testFileName)
 
     // return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
-    expect(fs.writeData).toBeCalled();
+    // expect(fs.writeData).toBeCalled();
+    expect(fs.writeData).toBeCalledWith(testFileName, testData);
 });
